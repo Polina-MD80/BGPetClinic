7c229ac23aa6d24785f46a46c77b559d77927b5c
Polina Pavlova <poli.paskaleva@gmail.com>
2021-09-01 14:33:22 +0000

    Create Project

---
+++ src/main/java/poly/springframework/bgpetclinic/BgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package poly.springframework.bgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/poly/springframework/bgpetclinic/BgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package poly.springframework.bgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
